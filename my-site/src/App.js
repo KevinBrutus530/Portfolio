30886c96753ba8d049fe1b8be5288de520fc9f1e
Kevin Brutus <kevinbrutus@pursuit.org>
2020-09-27 12:43:35 -0400

    add links, change favicon, title site

--- my-site/src/App.js
+++ my-site/src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Hello World</div>
+      <br />
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.linkedin.com/in/kevin-brutus/"
+      >
+        LinkedIn
+      </a>
+      <br />
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/KevinBrutus530"
+      >
+        GitHub
+      </a>
+      <br />
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://afro-link.netlify.app/"
+      >
+        Afro Link
+      </a>
+      <br />
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://tastebuds.netlify.app/"
+      >
+        Tastebuds
+      </a>
+      <br />
+      <a target="_blank" rel="noopener noreferrer" href="https://google.com/">
+        FingrSmash
+      </a>
     </div>
   );
 }
