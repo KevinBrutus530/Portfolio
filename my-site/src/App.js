ac7d55543751af52a51af74219202c787cb80b51
Kevin Brutus <kevinbrutus@pursuit.org>
2020-09-27 13:09:16 -0400

    deploy draft, begin styling

--- my-site/src/App.js
+++ my-site/src/App.js
@@ -1,46 +1,57 @@
 import React from "react";
 import "./App.css";
+import "./css/Brutus.css";
 
 function App() {
   return (
     <div className="App">
-      <div>Hello World</div>
-      <br />
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://www.linkedin.com/in/kevin-brutus/"
-      >
-        LinkedIn
-      </a>
-      <br />
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://github.com/KevinBrutus530"
-      >
-        GitHub
-      </a>
-      <br />
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://afro-link.netlify.app/"
-      >
-        Afro Link
-      </a>
-      <br />
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://tastebuds.netlify.app/"
-      >
-        Tastebuds
-      </a>
-      <br />
-      <a target="_blank" rel="noopener noreferrer" href="https://google.com/">
-        FingrSmash
-      </a>
+      <div className="mainContainer">
+        <div className="contactInfo">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.linkedin.com/in/kevin-brutus/"
+          >
+            LinkedIn
+          </a>
+        </div>
+        <div className="contactInfo">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/KevinBrutus530"
+          >
+            GitHub
+          </a>
+        </div>
+        <div className="projects">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://afro-link.netlify.app/"
+          >
+            Afro Link
+          </a>
+        </div>
+        <div className="projects">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://tastebuds.netlify.app/"
+          >
+            Tastebuds
+          </a>
+        </div>
+        <div className="projects">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://twitter.com/"
+          >
+            FingrSmash
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
